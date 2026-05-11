326553f838080a0b8c506729be2de15e5c9e8999
VuHanh0583 <vuh7993@gmail.com>
2026-05-11 11:07:40 +0700

    hoanthanh 07

---
+++ lab-01/ex02_07.py
@@ -0,0 +1,10 @@
+print("nhap cac dong van ban ( nhap 'done' de ket thuc): ")
+lines = []
+while True:
+    line = input()
+    if line == "done":
+        break
+    lines.append(line)
+print("\n cac dong sau nhap chuyen thanh chu hoa: ")
+for line in lines:
+    print(line.upper())
