9b338a285c323880febc45f4da4cc975874b2d00
HanhVu0583 <vuh7993@gmail.com>
2026-05-18 11:13:13 +0700

    Delete lab-01 directory

--- lab-01/ex02_01.py
+++
@@ -1,4 +0,0 @@
-#nhap ten va tuoi cua nguoi dung
-ten = input("nhap ten cua ban: ")
-tuoi = input("nhap tuoi cua ban: ")
-print("chao mung,", ten, "! ban", tuoi, "tuoi.")
--- lab-01/hello.py
+++
@@ -1,3 +0,0 @@
-print("hello,world!")
-print("my name is Hanh")
-print("hutech university")
